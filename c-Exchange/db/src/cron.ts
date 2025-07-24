8a60741130087481494be096f9685a4a6e1f979b
sayandas-sd <sayandasmac@gmail.com>
2025-07-24 20:09:58 +0530

    feat(db): all trades store into db

---
+++ c-Exchange/db/src/cron.ts
@@ -0,0 +1,27 @@
+import { Client } from 'pg'; 
+
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'mysecretpassword',
+    port: 5432,
+});
+
+client.connect();
+
+async function refreshViews() {
+
+    await client.query('REFRESH MATERIALIZED VIEW klines_1m');
+    await client.query('REFRESH MATERIALIZED VIEW klines_1h');
+    await client.query('REFRESH MATERIALIZED VIEW klines_1w');
+
+    console.log("refreshed successfully");
+}
+
+refreshViews().catch(console.error);
+
+setInterval(() => {
+    refreshViews()
+}, 1000 * 10 );
