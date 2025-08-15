994ee5c9617caccf7ed88ee267f3647455d2b0b2
sayandas-sd <sayandasmac@gmail.com>
2025-08-15 19:01:05 +0530

    fix: singleton ws connection

--- c-Exchange/ws/src/index.ts
+++ c-Exchange/ws/src/index.ts
@@ -1,8 +1,9 @@
 import { WebSocketServer } from "ws";
 import { UserManager } from "./UserManager";
 
-const wss = new WebSocketServer({ port: 3001 });
 
-wss.on("connection", (ws) => {
+const wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', function connection(ws) {
     UserManager.getInstance().addUser(ws);
-});
+})
